e913b5056af71894780621c768da4e1d8b5d07ca
Shawn Woodford <shawn@kingandpartners.com>
2014-11-05 17:45:46 -0500

    avoid git config requirement

--- install-s3cmd.sh
+++ install-s3cmd.sh
@@ -4,7 +4,7 @@
 # Test if already installed
 command -v s3cmd >/dev/null 2>&1 || {
 	cd ~
-	git clone git@github.com:s3tools/s3cmd.git
+	git clone https://github.com/s3tools/s3cmd.git
 	cd s3cmd
 	sudo python setup.py install
 	read -rp "Configure s3cmd? (y/n) " CONFIGURE
